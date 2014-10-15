c1b62942255ec4b0264897e3d29f45d9107ce9ae
mukaiizawa <mukaiizawa@yahoo.co.jp>
2014-10-15 22:35:53 +0900

    change symbolic.bat

--- symbolic.bat
+++ symbolic.bat
@@ -9,13 +9,14 @@ if /i {%ANSWER%}=={no} (goto :no)
 echo It is interrupted. & pause > nul & exit
 
 :yes
+REM N.B mklink:  to  from
 mklink /D "C:\users\user-name\.vim"        "C:\users\user-name\dotfiles\.vim"
 mklink /D "C:\users\user-name\vimfiles"    "C:\users\user-name\dotfiles\.vim"
 mklink    "C:\users\user-name\_vimrc"      "C:\users\user-name\dotfiles\.vimrc"
 mklink    "C:\users\user-name\_gvimrc"     "C:\users\user-name\dotfiles\.gvimrc"
 mklink    "C:\users\user-name\.vimshrc"    "C:\users\user-name\dotfiles\.vimshrc"
-mklink    "C:\users\user-name\.gitconfig"  "C:\users\user-name\dotfiles\.gitconfig"
 mklink    "C:\users\user-name\.gitignore"  "C:\users\user-name\dotfiles\.gitignore"
+REM mklink    "C:\users\user-name\.gitconfig"  "C:\users\user-name\dotfiles\.gitconfig"
  
 echo It finished. & pause > nul & exit
 
