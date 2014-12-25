afe42a3679ba03733ccd6bea9307a6d61f7084bd
mukaiizawa <mukaiizawa@yaoo.co.jp>
2014-12-25 23:34:05 +0900

    add

---
+++ symbolic.sh
@@ -0,0 +1,13 @@
+# Note: Execute this.
+#   mod +x symbolic.sh
+
+# Note: ln:  (from) (to)
+ln -s ~/dotfiles/.vim       ~/.vim
+ln -s ~/dotfiles/.vim       ~/vimfiles
+ln -s ~/dotfiles/.vimrc     ~/.vimrc
+ln -s ~/dotfiles/.gvimrc    ~/.gvimrc
+ln -s ~/dotfiles/.vimshrc   ~/.vimshrc
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+
+
+#ln -s ~/dotfiles/.minvimrc ~/.vimrc
