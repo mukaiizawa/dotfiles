521135c3e63d4befb21c813cb8d8710461726834
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-04 08:17:37 +0900

    Add ccl init file

---
+++ .ccl-init.lisp
@@ -0,0 +1,11 @@
+
+(setq *print-pretty* t)
+(defparameter *module-ahead-reader*)
+(defparameter *module-database-manager*)
+(defparameter *module-date-utils*)
+(defparameter *module-graph-utils*)
+(defparameter *module-regex*)
+(defparameter *module-stdlib*)
+(defparameter *module-test-utils*)
+(defparameter *module-xml-manager*)
+
