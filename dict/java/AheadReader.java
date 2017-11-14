0d4334feb4af00f3cb7b55441c97b21538e14603
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-11-14 17:19:53 +0900

    Remove unused library

--- dict/java/AheadReader.java
+++ dict/java/AheadReader.java
@@ -3,7 +3,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
