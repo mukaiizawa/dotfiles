b8242c5996bb19462e0205efd95319972d55bb5d
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-02-08 20:22:10 +0900

    Update nqueen problem

--- dict/java/NQueenProblem.java
+++ dict/java/NQueenProblem.java
@@ -1,90 +1,50 @@
-/**
- * N queen problem.
+/*
+ * n queen problem
  */
+
 public class NQueenProblem {
 
-  // status of board
-  public static int BOARD_SIZE = 8;
-  public static int BOARD_FREE = 0;
-  public static int BOARD_QUEEN = 1;
+  private static final int BOARD_SIZE = 8;
+  private static boolean[][] board = new boolean[BOARD_SIZE][BOARD_SIZE];
 
-  // for print
-  public static String BOARD_CHAR = " ";
-  public static String QUEEN_CHAR = "Q";
-
-  public static int PARTICULAR_SOLUTION_NUM = 0;
-
-  public static void main(String[] args) {
-    tryPut(createBoard(BOARD_SIZE), 0);
-    System.out.println(PARTICULAR_SOLUTION_NUM);
-    return;
-  }
-
-  // tryPut is the function that try put Queen line `n' on a `board'.
-  public static void tryPut(int[][] board, int n) {
-    if (n == BOARD_SIZE) {
-      PARTICULAR_SOLUTION_NUM++;
-      showBoard(board);
-      return;
-    }
-    for (int i = 0; i < BOARD_SIZE; i++)
-      if (canPutQueen(board, n, i))
-        tryPut(putQueen(board, n, i), n + 1);
-    return;
-  }
-
-  public static boolean canPutQueen(int[][] board, int row, int col) {
-    if (!isInBoard(row, col)) return false;
-    // row, col
-    for (int i = 0; i < BOARD_SIZE; i++)
-      if (board[row][i] == BOARD_QUEEN || board[i][col] == BOARD_QUEEN)
-        return false;
-    // down-right
-    for (int i = row + 1, j = col + 1; isInBoard(i, j); i++, j++)
-      if (board[i][j]==BOARD_QUEEN) return false;
-    // down-left
-    for (int i = row + 1, j = col - 1; isInBoard(i, j); i++, j--)
-      if (board[i][j]==BOARD_QUEEN) return false;
-    // top-right
-    for (int i = row - 1, j = col + 1; isInBoard(i, j); i--, j++)
-      if (board[i][j] == BOARD_QUEEN) return false;
-    // top-left
-    for (int i = row - 1, j = col - 1; isInBoard(i, j); i--, j--)
-      if (board[i][j] == BOARD_QUEEN) return false;
-    return true;
-  }
-
-  public static int[][] putQueen(int[][] board, int row, int col) {
-    int [][] result = new int[BOARD_SIZE][BOARD_SIZE];
+  private static void printBoard() {
     for (int i = 0; i < BOARD_SIZE; i++) {
-      for (int j = 0; j < BOARD_SIZE; j++) {
-        result[i][j] = (i == row && j == col)?
-          BOARD_QUEEN:
-          board[i][j];
-      }
-    }
-    return result;
-  }
-
-  public static boolean isInBoard(int row, int col) {
-    return (0 <= row&&row < BOARD_SIZE) && (0 <= col&&col < BOARD_SIZE);
-  }
-
-  public static int[][] createBoard(int n) {
-    return new int[8][8];
-  }
-
-  public static void showBoard(int[][] board) {
-    for (int[] line: board) {
-      for (int cell: line) {
-        System.out.print((BOARD_QUEEN==cell)?
-            QUEEN_CHAR:
-            BOARD_CHAR);
-      }
+      for (int j = 0; j < BOARD_SIZE; j++)
+        System.out.print(board[i][j]? " Q": " -");
       System.out.println();
     }
     System.out.println();
-    return;
+  }
+
+  private static boolean isInBoard(int row, int col) {
+    return 0 <= row && row < BOARD_SIZE && 0 <= col && col < BOARD_SIZE;
+  }
+
+  private static boolean canPut(int row, int col) {
+    for (int i = row; i >= 0; i--)
+      if (board[i][col]) return false;
+    for (int i = row, j = col; isInBoard(i, j); i--, j--)
+      if (board[i][j]) return false;
+    for (int i = row, j = col; isInBoard(i, j); i--, j++)
+      if (board[i][j]) return false;
+    return true;
+  }
+
+  private static void tryPut(int row) {
+    if (row == BOARD_SIZE) printBoard();
+    else {
+      for (int col = 0; col < BOARD_SIZE; col++) {
+        if (canPut(row, col)) {
+          board[row][col] = true;
+          tryPut(row + 1);
+          board[row][col] = false;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    tryPut(0);
   }
 
 }
