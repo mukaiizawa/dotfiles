1738770661a30abf334766f3889713c6e8dd45c6
mukaiizawa <mukaiizawa@gmail.com>
2020-10-24 15:22:20 +0900

    Update

--- dict/java/run.bat
+++ dict/java/run.bat
@@ -2,28 +2,8 @@
 CD /D %~DP0
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-GOTO main
-
-:usage
-ECHO.
-ECHO run FILE
-ECHO 	指定したjavaファイルをコンパイルし、実行します。
-ECHO 	実行するにはjdkへのパスが通っている必要があります。
-ECHO 	FILE -- コンパイルして実行するjavaファイル
-ECHO.
-ECHO 使用例
-ECHO run Test
-ECHO.
-GOTO end_batch
-
-:main
-IF "x%1"=="x" (
-  GOTO usage
-)
-
-javac -encoding utf8 %1.java
-java %1
+REM set CLASSPATH=.;C:/foo/bar/xxx.jar
+javac -encoding utf8 x.java
+java x
 
 pause
-
-:end_batch
