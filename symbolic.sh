943cb0b16eb44fb2db33b65903a1339ec3bab018
Shintaro Mukai <mukaiizawa@gmail.com>
2021-08-02 17:12:59 +0900

    Minor changes

--- symbolic.sh
+++ symbolic.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# Note: Execute this.
-# chmod +x symbolic.sh
-
-# Note: ln:  (from) (to)
-ln -s ~/dotfiles/.vim       ~/.vim
-ln -s ~/dotfiles/.vim       ~/vimfiles
-ln -s ~/dotfiles/bin       ~/userbin
-ln -s ~/dotfiles/.vimrc     ~/.vimrc
-ln -s ~/dotfiles/.gvimrc    ~/.gvimrc
-ln -s ~/dotfiles/.vimshrc   ~/.vimshrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vim ~/vimfiles
+ln -s ~/dotfiles/bin ~/userbin
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.vimshrc ~/.vimshrc
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.ccl-init.lisp ~/.ccl-init.lisp
-#ln -s ~/dotfiles/.minvimrc ~/.vimrc
