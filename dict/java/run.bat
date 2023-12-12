b074e8635444fef6a1559e09b0dbacbbcb433085
Shintaro Mukai <mukaiizawa@gmail.com>
2023-12-12 18:07:42 +0900

    Update

--- dict/java/run.bat
+++ dict/java/run.bat
@@ -1,7 +1,9 @@
 @ECHO off
 CD /D %~DP0
-SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM set CLASSPATH=.;C:/foo/bar/xxx.jar
 javac -encoding utf8 Main.java
 java Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+REM JEP 330: Launch Single-File Source-Code Programs
+java Main.java %1 %2 %3 %4 %5 %6 %7 %8 %9
