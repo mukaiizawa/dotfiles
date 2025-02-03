5a79a75a41ae251f00694d3c280265dd64ce030b
Shintaro Mukai <shintaro@mukai.systems>
2025-02-03 14:14:57 +0900

    u

---
+++ dict/cs/run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set csc=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe 
+del *.exe > nul 2>&1
+
+%csc% /nologo /out:run.exe /main:Main Main.cs Lib1.cs Lib2.cs
+
+IF %ERRORLEVEL% == 0 (
+  run
+)
