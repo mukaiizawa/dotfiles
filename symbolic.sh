42c26e507f6d43c073f7e2619c377afaec4200f8
Shintaro Mukai <shintaro@mukai.systems>
2025-02-22 07:43:48 +0900

    Fixed to avoid unnecessary symbolic links.

--- symbolic.sh
+++ symbolic.sh
@@ -2,9 +2,6 @@
 
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vim ~/vimfiles
-ln -s ~/dotfiles/bin ~/userbin
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.vimshrc ~/.vimshrc
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.ccl-init.lisp ~/.ccl-init.lisp
