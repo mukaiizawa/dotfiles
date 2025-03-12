a0945e637b6e96d93f14243f2656811403b7e14e
Shintaro Mukai <shintaro@mukai.systems>
2025-03-12 17:01:09 +0900

    u

--- dict/cs/run.bat
+++ dict/cs/run.bat
@@ -5,6 +5,9 @@ del *.exe > nul 2>&1
 
 %csc% /nologo /out:run.exe /main:Main Main.cs Lib1.cs Lib2.cs
 
+REM with reference
+REM %csc% /nologo /out:run.exe /reference:System.Data.SQLite.dll /main:Main Main.cs Lib1.cs Lib2.cs
+
 IF %ERRORLEVEL% == 0 (
   run
 )
