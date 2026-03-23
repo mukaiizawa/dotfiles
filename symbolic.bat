94097607c029868d9cdb4c5dcc53ac90ed318f43
Shintaro Mukai <shintaro@mukai.systems>
2026-03-23 15:22:56 +0900

    u

--- symbolic.bat
+++ symbolic.bat
@@ -1,7 +1,7 @@
 @echo off
 rem n.b mklink:  (to)  (from)
 echo "%HOMEDRIVE%%HOMEPATH%\.vim"
-mklink /D "%HOMEDRIVE%%HOMEPATH%\.claude"     "%HOMEDRIVE%%HOMEPATH%\dotfiles\.claude"
+mklink /D "%HOMEDRIVE%%HOMEPATH%\.claude"  "%HOMEDRIVE%%HOMEPATH%\dotfiles\.claude"
 mklink /D "%HOMEDRIVE%%HOMEPATH%\.vim"     "%HOMEDRIVE%%HOMEPATH%\dotfiles\.vim"
 mklink /D "%HOMEDRIVE%%HOMEPATH%\vimfiles" "%HOMEDRIVE%%HOMEPATH%\dotfiles\.vim"
 mklink    "%HOMEDRIVE%%HOMEPATH%\_vimrc"   "%HOMEDRIVE%%HOMEPATH%\dotfiles\.vimrc"
