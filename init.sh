7ae9bd2733d3f22f1131a2c43e8a914544ecaf71
Shintaro Mukai <shintaro@mukai.systems>
2026-07-15 14:11:11 +0900

    Update symbolic link

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -s ~/dotfiles/.claude ~/.claude
+ln -s ~/dotfiles/.codex ~/.codex
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vim ~/vimfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.ccl-init.lisp ~/.ccl-init.lisp
 cp ~/dotfiles/.vimrc.local ~/.vimrc.local
